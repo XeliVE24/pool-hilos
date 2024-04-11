bbe7be0754ff914ebc7bd0361f81388794c8775b
Xeli Vargas <xeli.vargas@uteca.edu.mx>
2024-04-11 08:47:55 -0600

    :)

--- tarea.py
+++ tarea.py
@@ -14,9 +14,10 @@ t0=time.time()
 
 globalArrayNum=[]
 rangos=[(1,50),(51,100),(101,150),(151,200)]
-with ThreadPoolExecutor(max_workers=2) as executor:
-    for i ,rango in enumerate (rangos):
-        t=executor.submit(contadorDos, rango)
+rango=len(rangos)
+with ThreadPoolExecutor(max_workers=min(rango,2)) as executor:
+    for rango in rangos:
+        t=executor.submit(contadorDos, rango[0],rango[1])
   
 tf=time.time()-t0
 print(f"tiempo total de ejecucion:{tf}")
